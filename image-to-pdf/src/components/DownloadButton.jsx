76ca5e51cf8a7bda683d51862e3e1061b51a9dab
Dhanunjaya Guptha Amaravathi <amaravathi.gupta@ip-10-227-226-149>
2025-06-20 11:30:22 +0530

    add green color to download button

--- image-to-pdf/src/components/DownloadButton.jsx
+++ image-to-pdf/src/components/DownloadButton.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+
 
 const DownloadButton = ({ url }) => (
   url ? (
     <a href={url} download="converted.pdf">
-      <button className="convert-button">Download PDF</button>
+      <button className="bg-green-500 text-white px-4 py-2 rounded">Download PDF</button>
     </a>
   ) : null
 );
